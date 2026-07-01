8de58fed3484e57b4da36f6f3a7034ef9369ba71
sivashiri <sivashirimurugan126@gmail.com>
2026-07-01 15:36:50 +0530

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+from streamlit_calendar import calendar
+
+
+# ---------------------------------------------------
+# STATUS COLORS
+# ---------------------------------------------------
+
+STATUS_COLORS = {
+    "Planned
